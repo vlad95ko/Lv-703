baf197a19e7ec7540670626de5ab784d38f2f181
tarasavage <tarasuser9@gmail.com>
2022-06-25 19:35:32 +0300

    The first homework by Taras Panasiuk.

--- HW1.cs
+++ HW1.cs
@@ -21,15 +21,14 @@ namespace Homework
             age = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"Hello {name}, you are {age} years old.");
 
-
             //Task 3
-            double r;
+            double R;
             const double PI = 3.14;
             Console.WriteLine("Enter the radius of a circle");
-            r = Convert.ToDouble(Console.ReadLine());
-            double length = 2 * PI * r;
-            double area = PI * r * r;
-            double volume = 4.0 / 3.0 * PI * r * r * r;
+            R = Convert.ToDouble(Console.ReadLine());
+            double length = 2 * PI * R;
+            double area = PI * R * R;
+            double volume = 4.0 / 3.0 * PI * R * R * R;
             Console.WriteLine($"Length of a circle = {length:F2}, area = {area:F2} and volume = {volume:F2}");
         }
     }
