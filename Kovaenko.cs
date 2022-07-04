d472862e8d580cc5a1e556fe8b7ba76e80c91c1a
vlad95ko <vladyslav.a.kovalenko@gmail.com>
2022-07-04 16:38:29 +0800

    KovalenkoHW4

---
+++ Kovaenko.cs
@@ -0,0 +1,31 @@
+﻿namespace HW_4_Kovalenko
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Tom", 1980);
+            Person[] person = new Person[6];
+            for (int i = 1; i < person.Length; i++)
+            {
+                person[0] = person1;
+                person[i] = Person.Input(i);
+            }
+            for (int i = 0; i < person.Length; i++)
+            {
+                person[i].Age();
+                person[i].ChangeName();
+                person[i].Output();
+            }
+            for(int i = 0; i < person.Length; i++)
+            {
+                for (int j = i + 1; j < 6; j++)
+                {
+                    if (person[i] == person[j])
+                    { Console.WriteLine($"person {i} same names person {j}"); }
+                }
+            }
+
+        }
+    }
+}
