cc3d37fea81937bccb03e5bc97a707d5ef0aa217
Dmytro <dskpochta@gmail.com>
2022-07-04 13:02:05 +0300

    Add Task & Homework 4

---
+++ Kymachynskyi Dmytro/Homework_4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            var persons = new Person[6];
+            persons[0] = person;
+            for (int i = 1; i < persons.Length; i++) 
+                persons[i] = person.Input();
+            Console.WriteLine("\nOutputting persons data...");
+            for (int i = 0; i < persons.Length; i++) 
+                person.Output(persons[i]);
+            Console.WriteLine("\nChecking age to rename persons...");
+            for (int i = 0; i < persons.Length; i++)
+                if (person.Age(persons[i]) < 16) person.ChangeName(persons[i]);
+            Console.WriteLine("\nOutputting persons data...");
+            for (int i = 0; i < persons.Length; i++) 
+                person.Output(persons[i]);
+            Console.WriteLine("\nFinding persons with same names...");
+            bool foundFlag = false;
+            for (var i = 0; i < persons.Length; i++)
+            {
+                for (int j = i+1; j < persons.Length; j++)
+                {
+                    if (persons[i] == persons[j])
+                    {
+                        person.Output(persons[i]);
+                        Console.WriteLine("have the same name to:");
+                        person.Output(persons[j]);
+                        Console.WriteLine();
+                        foundFlag = true;
+                    }
+                }
+            }
+            if (!foundFlag) Console.WriteLine("\nNot found persons with same names!");
+        }
+    }
+
+}
