c99dc263644449e575b0c3b978d9f86d9747cc95
UnseenHand <95188996+UnseenHand@users.noreply.github.com>
2022-07-07 14:23:31 +0300

    ADD:
    Homework5,
    IDeveloper,
    Builder,
    Programmer

---
+++ Homework1UgeraVitaliy/Builder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Homework5
+{
+    internal class Builder : IDeveloper
+    {
+        public string tool;
+        public string Tool
+        {
+            get { return tool; }
+            set { tool = value; }
+        }
+        public Builder(string tool)
+        {
+            this.tool = tool;
+        }
+        public int CompareTo(IDeveloper other)
+        {
+            return this.Tool.CompareTo(other.Tool);
+        }
+        public void Create()
+        {
+            Console.WriteLine("Created : {0}", this.Tool);
+        }
+        public void Destroy()
+        {
+            Console.WriteLine("Destroyed : {0}, but just in words", this.Tool);
+        }
+    }
+}
---
+++ Homework1UgeraVitaliy/IDeveloper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Homework5
+{
+    internal interface IDeveloper : IComparable<IDeveloper>
+    {
+        string Tool { get; set; }
+        void Create();
+        void Destroy();
+    }
+}
